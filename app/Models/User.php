347b13153ef24bb5cd3b527bc81e61ebb1cb0545
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-09-20 17:05:06 +0300

    refactor: changes values for hidden array

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
@@ -33,6 +34,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'role',
+        'active',
+        'email',
+        'created_at',
+        'updated_at'
     ];
 
     /**
