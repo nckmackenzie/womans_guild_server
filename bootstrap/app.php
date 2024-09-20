708abe8d4fa97fe6877440d05640a25d3474f55c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-09-20 17:04:47 +0300

    refactor: register customauth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\ConvertCaseMiddleware;
+use App\Http\Middleware\CustomAuthMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -22,6 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->append(ConvertCaseMiddleware::class);
+        // $middleware->append(CustomAuthMiddleware::class);
 
         //
     })
