fef67ceafae7fc7033caceaf52db1022be6ad944
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-09-20 08:16:32 +0300

    refactor: add authenicated controller

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,13 @@ use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\VoteheadController;
 use App\Http\Controllers\Api\YearController;
 use App\Http\Controllers\Api\YearlyContributionController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+                ->middleware('guest')
+                ->name('login');
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
