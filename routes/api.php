43edad98da060f53166ad969522b2a1f6a27aaec
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-09-23 16:09:28 +0300

    comment out

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->withoutMiddleware([CustomAuthMiddleware::class]);
 
-// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-//     // return $request->user();
-//     return ['Laravel' => app()->version()];
-// });
-
 Route::middleware([CustomAuthMiddleware::class])->group(function(){
     Route::get('/user', function (Request $request) {
         // return response()->json(['data' => $request->user()]);
@@ -56,6 +51,11 @@ Route::middleware([CustomAuthMiddleware::class])->group(function(){
         Route::get('/reports/budgetExpense','budgetExpense');
     });
 });
+
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     // return $request->user();
+//     return ['Laravel' => app()->version()];
+// });
 
 // Route::middleware('auth:sanctum')->group(function(){
 //     Route::post('/logout', [AuthController::class, 'logout']);
