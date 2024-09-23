eb07fc2f791a3758302379d39382ec17614444df
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2024-09-23 08:22:30 +0300

    feat: migration schema for income projections table

---
+++ database/migrations/2024_09_22_114343_create_income_projection_headers_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('income_projection_headers', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('year_id');
+            
+            $table->foreign('year_id')->references('id')->on('years')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('income_projection_headers');
+    }
+};
