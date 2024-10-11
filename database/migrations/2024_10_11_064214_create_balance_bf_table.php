53381146a339d996298017860628daccf854c83c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-10-11 10:09:45 +0300

    feat: table for bfs migration

---
+++ database/migrations/2024_10_11_064214_create_balance_bf_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('balance_bf', function (Blueprint $table) {
+            $table->id();
+            $table->string('year_id');
+            $table->string('member_id');
+            $table->decimal('amount', 10, 2)->default(0.00);
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade');
+            $table->foreign('year_id')->references('id')->on('years')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('balance_bf');
+    }
+};
