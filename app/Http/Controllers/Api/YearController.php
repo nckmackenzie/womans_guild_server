79cb4404a9623d4dce145a02922f78ba936a1a0d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-11-06 11:02:33 +0300

    refactor: sort active years by date created

--- app/Http/Controllers/Api/YearController.php
+++ app/Http/Controllers/Api/YearController.php
@@ -92,7 +92,7 @@ class YearController extends Controller
 
     public function activeYears()
     {
-        $years = Year::where('is_closed',0)->select('id','name')->get();
+        $years = Year::where('is_closed',0)->select('id','name')->orderBy('created_at','desc')->get();
         return response()->json(['data' => $years]);
     }
 }
